24ab2cb986efeb01d5a95910e890cdbc30f914bf
Abi <abinaya2260@gmail.com>
2026-04-12 01:58:46 +0530

    written code in even_odd

---
+++ basic problems/0.All_topics.py
@@ -0,0 +1,21 @@
+"""
+
+1.Even or Odd
+2.Sum of list (without sum)
+3.Count vowels
+4.Reverse a string
+
+5.Palindrome check
+6.Remove duplicates
+7.Factorial
+8.Count words
+
+9.First non-repeating character
+10.FizzBuzz
+11.Second -largest number
+
+12.Two Sum
+13.Find duplicates
+14.Anagram check
+
+"""
