9d82fd3a36fca3ed62f2a0d39de1ae7b6d427363
Abi <abinaya2260@gmail.com>
2026-05-11 00:04:48 +0530

    learned merge two sorted arrays problem

---
+++ Leetcode/5.Merge_two_sorted_arrays  21.py
@@ -0,0 +1,77 @@
+# Merge two sorted arrays: concept: Linked List
+#Approach: Two pointer
+
+"""
+Algorithm:
+
+1. create a dummy node
+2.create a pointer tail and point it to dummy
+3.Traverse both linked lists until one becomes empty
+4.compare values of both nodes:
+        * If list1 value is smaller:
+                * connect tail.next to list1
+                * move list1 forward
+        * Else:
+                * connect tail.next to list2
+                * move list2 forward
+5. Move tail forward
+6.After loop ends:
+        * if nodes remain in list1 , attach them
+        * if nodes remain in list2 , attach them
+7. Return dummy.next
+
+Time Complexity: o(n+m) ->  we visit each node: if n=length of list1 and m = length of list2
+Space Complexity: o(1) -> because no extra list is created
+
+"""
+
+
+class ListNode:
+    def __init__(self,val=0,next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+
+        dummy = ListNode()      #dummy node
+        tail = dummy            #pointer to build new list
+        while list1 and list2:  #compare both lists
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next       # move tail forward
+        if list1:                  # if any nodes remain
+            tail.next = list1
+        else:
+            tail.next = list2
+        return dummy.next           # return merged list
+
+
+
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list.
+The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+"""
